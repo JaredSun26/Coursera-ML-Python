2202c17d3925cb9606c3a6e7a175785fff63df58
Zhe(Jared) Sun <jaredsun26@gmail.com>
2016-08-01 18:15:58 -0700

    Add instructions and warmup file

---
+++ Week1/warmup.py
@@ -0,0 +1,7 @@
+#WARMUPEXERCISE Example function in octave
+#   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+import numpy as np
+def warmupexercise():
+    return np.eye(5)
+
+print(warmupexercise())
